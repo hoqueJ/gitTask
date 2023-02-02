c7238ba29b4e308d7993753e7da2d66b2a06cb95
Minhaj Hoque <minhaj@MINHAJ-PC.localdomain>
2023-02-02 04:21:02 +0000

    New file helloWorld.js created, its message is changed to 'Git is Awesome'

--- helloWorld.js
+++ helloWorld.js
@@ -1,4 +1,8 @@
 // T43 - Compulsory Task 1
 
-let message = "Git is Awesome!"
-console.log(message);
+
+let message = "Hello World!";   // variable assigned the message "Hello World"
+
+message = "Git is Awesome!"; // variable is reassigned a new message
+
+console.log(message);   // the message is then printed out
